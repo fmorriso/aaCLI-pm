761702252b96df8012cb13edda70b6ee66ade26c
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2021-05-28 16:45:12 -0400

    update to Angular 12.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, VERSION as ngVersion} from '@angular/core';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'aaCLI-pm';
+  public get angularVersion(): string {
+    return ngVersion.full;
+  }
 }
